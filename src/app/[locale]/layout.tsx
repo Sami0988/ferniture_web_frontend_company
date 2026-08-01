ce5966435052c752905de8280bb6629d3404c26d
samuel zelalem <zsami9017@gmail.com>
2026-08-01 16:26:35 +0300

    Add Google site verification

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -61,7 +61,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    google: 'your-google-verification-code',
+    google: '1e0NqrRCqpnpA74uy0aritx29ewHSA4fyC_Z7KrIPXI',
   },
 };
 
